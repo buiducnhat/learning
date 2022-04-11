025e2ed1188e2aad7533c9d88591b4b4d856d5a8
gerpan <nhaths4701@gmail.com>
2022-04-11 14:45:29 +0700

    Add android exercise

--- android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "Login button clicked");
+                showToast(v);
+            }
+        });
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Hello my friend", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
